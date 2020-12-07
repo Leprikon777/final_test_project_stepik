33f6f504835d3bd5054d41cbc65b297241bcb923
a.grizenko <a.grizenko@digital-sector.ru>
2020-12-07 16:00:56 +0300

    Добавили переход на страницу логин/регистрация. Проверку корректности url наличия форм логина, регистрации. Также закомментированый код клик по алерту

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,7 +10,7 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         #assert self.is_element_present(*LoginPageLocators.LOGIN_URL)
-        assert self.getCurrentUrl() == "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        assert self.browser.current_url == "http://selenium1py.pythonanywhere.com/ru/accounts/login/", f"{self.browser.current_url}"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM)
